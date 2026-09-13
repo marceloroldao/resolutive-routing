3793a202c7f44d18ce2e66a9df2aea73048a155e
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-09-13 17:10:45 -0300

    chore: export rerouting and route quality primitives

--- src/resolutive_routing/__init__.py
+++ src/resolutive_routing/__init__.py
@@ -2,11 +2,24 @@
 
 from .contracts import Node, Request, RequestType, RouteDecision, Scope
 from .ledger import CreditLedger, ResourceType
+from .metrics import RouteOutcome, RouteQuality, evaluate_route_quality
+from .reroute import FailureEvent, RerouteDecision, reroute_after_failure
 from .router import DeterministicRouter
 
 __all__ = [
-    "CreditLedger", "DeterministicRouter", "Node", "Request",
-    "RequestType", "ResourceType", "RouteDecision", "Scope",
+    "CreditLedger",
+    "DeterministicRouter",
+    "FailureEvent",
+    "Node",
+    "Request",
+    "RequestType",
+    "ResourceType",
+    "RerouteDecision",
+    "RouteDecision",
+    "RouteOutcome",
+    "RouteQuality",
+    "Scope",
+    "evaluate_route_quality",
+    "reroute_after_failure",
 ]
-__version__ = "0.1.0"
-
+__version__ = "0.1.1"
