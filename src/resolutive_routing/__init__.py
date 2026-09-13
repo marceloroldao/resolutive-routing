8c12e8d49204f767813c560ee173ea32525b4234
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-09-13 18:38:07 -0300

    chore: export MA2A adapter primitives

--- src/resolutive_routing/__init__.py
+++ src/resolutive_routing/__init__.py
@@ -2,6 +2,7 @@
 
 from .contracts import Node, Request, RequestType, RouteDecision, Scope
 from .ledger import CreditLedger, ResourceType
+from .ma2a_adapter import failure_from_ma2a_notice, node_from_ma2a_capability
 from .metrics import RouteOutcome, RouteQuality, evaluate_route_quality
 from .reroute import FailureEvent, RerouteDecision, reroute_after_failure
 from .router import DeterministicRouter
@@ -20,6 +21,8 @@ __all__ = [
     "RouteQuality",
     "Scope",
     "evaluate_route_quality",
+    "failure_from_ma2a_notice",
+    "node_from_ma2a_capability",
     "reroute_after_failure",
 ]
 __version__ = "0.1.0"
